0182dad988d60310f2ae38abff0e9cf202e491e7
nazneen2203 <105688821+nazneen2203@users.noreply.github.com>
2022-06-03 23:41:06 -0400

    Update app.js

--- config/app.js
+++ config/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
+//var usersRouter = require('../routes/users');
 var aboutRouter = require('../routes/about');
 var contactRouter = require('../routes/contact');
 var projectsRouter = require('../routes/projects');
